1505b952ba32d1ea405052a9039c9edc24519cd4
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-02 18:15:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,10 +108,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     :alan_kay[:languages] = "GUI"
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+     alans_new_info = programmer_hash[:alan_kay][:languages] = "GUI"
+     return alans_new_info
      
 end
 
