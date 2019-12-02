04e01ed302db4e205929392b62c77d9f83633bd0
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-02 18:20:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,7 +129,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     ritchies_new_info = programmer_hash[:dennis_ritchie][:known_for][1] = "Assembly"
+     ritchies_new_info = programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
      return programmer_hash
 
 
